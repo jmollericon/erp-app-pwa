b2e04c536d963e1c863a2a5da7610b7d9902c112
Jorge Mollericon <jlmollericon@gmail.com>
2020-12-14 11:42:54 -0400

    test sw

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js')
-    .then(reg => console.log('Registro de SW exitoso', reg))
-    .catch(err => console.warn('Error al tratar de registrar el sw', err))
+    .then(reg => {
+      console.log('Registro de SW exitoso', reg)
+      $('#msj').text('Registro de SW exitoso')
+    }
+    )
+    .catch(err => {
+      console.warn('Error al tratar de registrar el sw', err)
+      $('#msj').text('Error al tratar de registrar el sw')
+    })
 }
